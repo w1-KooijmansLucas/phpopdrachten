6e09e8ae2259bbd0b92edef5fa73542749134da8
Lucas <lucas.kooijmans@edu-kw1c.nl>
2020-02-08 16:27:11 +0100

    Opdracht 2.1 met daarbijhorende standaard layout

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * User: Lucas Kooijmans
+ * Date: 8-2-2020
+ * Time: 14:58
+ * File: index.php
+ */
+?>
+
+<head>
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <header>
+        <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+    </header>
+    <aside>
+        <h2>Menu</h2>
+        <ul>
+            <li>Hoofdstuk 2
+                <ul>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>Hoofdstuk 3
+                <ul>
+                    <li>
+                        <a href="hoofdstuk3/opdracht_3-1.php">Opdracht 3.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk3/opdracht_3-2.php">Opdracht 3.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk3/opdracht_3-3.php">Opdracht 3.3</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>Hoofdstuk 4
+                <ul>
+                    <li>
+                        <a href="hoofdstuk4/opdracht_4-1.php">Opdracht 4.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk4/opdracht_4-2.php">Opdracht 4.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk4/opdracht_4-3.php">Opdracht 4.3</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk4/opdracht_4-4.php">Opdracht 4.4</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <ul>
+            <li>Hoofdstuk 5
+                <ul>
+                    <li>
+                        <a href="hoofdstuk5/opdracht_5-1.php">Opdracht 5.1</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk5/opdracht_5-2.php">Opdracht 5.2</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk5/opdracht_5-3.php">Opdracht 5.3</a>
+                    </li>
+                    <li>
+                        <a href="hoofdstuk5/opdracht_5-4.php">Opdracht 5.4</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+    <main id="wrapper">
+        <h2>Uitwerkingen</h2>
+    </main>
+</body>
