3cd802cb21da9500d1c4d183bb7d989a9e3f799c
Lucas <lucas.kooijmans@edu-kw1c.nl>
2020-02-10 15:07:41 +0100

    a href'jes verbeterd

--- hoofdstuk2/opdracht_2-1.php
+++ hoofdstuk2/opdracht_2-1.php
@@ -20,10 +20,10 @@
         <li>Hoofdstuk 2
             <ul>
                 <li>
-                    <a href="hoofdstuk2/opdracht_2-1.php">Opdracht 2.1</a>
+                    <a href="opdracht_2-1.php">Opdracht 2.1</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk2/opdracht_2-2.php">Opdracht 2.2</a>
+                    <a href="opdracht_2-2.php">Opdracht 2.2</a>
                 </li>
             </ul>
         </li>
@@ -32,13 +32,13 @@
         <li>Hoofdstuk 3
             <ul>
                 <li>
-                    <a href="hoofdstuk3/opdracht_3-1.php">Opdracht 3.1</a>
+                    <a href="../hoofdstuk3/opdracht_3-1.php">Opdracht 3.1</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk3/opdracht_3-2.php">Opdracht 3.2</a>
+                    <a href="../hoofdstuk3/opdracht_3-2.php">Opdracht 3.2</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk3/opdracht_3-3.php">Opdracht 3.3</a>
+                    <a href="../hoofdstuk3/opdracht_3-3.php">Opdracht 3.3</a>
                 </li>
             </ul>
         </li>
@@ -47,16 +47,16 @@
         <li>Hoofdstuk 4
             <ul>
                 <li>
-                    <a href="hoofdstuk4/opdracht_4-1.php">Opdracht 4.1</a>
+                    <a href="../hoofdstuk4/opdracht_4-1.php">Opdracht 4.1</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk4/opdracht_4-2.php">Opdracht 4.2</a>
+                    <a href="../hoofdstuk4/opdracht_4-2.php">Opdracht 4.2</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk4/opdracht_4-3.php">Opdracht 4.3</a>
+                    <a href="../hoofdstuk4/opdracht_4-3.php">Opdracht 4.3</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk4/opdracht_4-4.php">Opdracht 4.4</a>
+                    <a href="../hoofdstuk4/opdracht_4-4.php">Opdracht 4.4</a>
                 </li>
             </ul>
         </li>
@@ -65,16 +65,16 @@
         <li>Hoofdstuk 5
             <ul>
                 <li>
-                    <a href="hoofdstuk5/opdracht_5-1.php">Opdracht 5.1</a>
+                    <a href="../hoofdstuk5/opdracht_5-1.php">Opdracht 5.1</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk5/opdracht_5-2.php">Opdracht 5.2</a>
+                    <a href="../hoofdstuk5/opdracht_5-2.php">Opdracht 5.2</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk5/opdracht_5-3.php">Opdracht 5.3</a>
+                    <a href="../hoofdstuk5/opdracht_5-3.php">Opdracht 5.3</a>
                 </li>
                 <li>
-                    <a href="hoofdstuk5/opdracht_5-4.php">Opdracht 5.4</a>
+                    <a href="../hoofdstuk5/opdracht_5-4.php">Opdracht 5.4</a>
                 </li>
             </ul>
         </li>
