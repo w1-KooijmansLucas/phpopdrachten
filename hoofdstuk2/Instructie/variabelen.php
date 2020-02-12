9113bc0b06a166f67cbb7923748742716f08d011
Lucas <lucas.kooijmans@edu-kw1c.nl>
2020-02-12 09:22:29 +0100

    instructie

---
+++ hoofdstuk2/Instructie/variabelen.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * User: Lucas Kooijmans
+ * Date: 12-2-2020
+ * Time: 8:48
+ * File: variabelen.php
+ */
+?>
+<head>
+    <meta charset="UTF-8">
+    <link href="" rel="stylesheet" type="text/css">
+    <title>
+        Instructie variabelen in PHP
+    </title>
+</head>
+<body>
+        <?php
+            //String
+            $car = "audi";
+
+            //Boolean
+            $license = true;
+
+            /*
+             * commentaar
+             */
+
+            //Integer (number)
+            $age = 24;
+
+            //Float
+            $price = 20000.99;
+
+            echo '<p class="red">' . $car . ' heeft een prijs van ' . $price . '</p>';
+        ?>
+</body>
