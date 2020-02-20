ac2bf11e1839abc91461d2e4605d19e7d0e2753c
Lucas <lucas.kooijmans@edu-kw1c.nl>
2020-02-20 17:16:33 +0100

    Header, menu en footer

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Lucas Kooijmans
+ * Date:
+ * Time:
+ * File: .php
+ */
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <link href="/phpopdrachten/css/style.css" rel="stylesheet" type="text/css">
+    <title>
+        Uitwerking van PHP-opdrachten
+    </title>
+</head>
+<body>
+<header>
+    <h1><?php echo "Uitwerking van PHP-opdrachten"; ?></h1>
+</header>
