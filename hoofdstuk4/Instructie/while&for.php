c280579e88c9b3dc8b032ab1872e62c72f8b6fdd
Lucas <lucas.kooijmans@edu-kw1c.nl>
2020-03-09 12:31:22 +0100

    instructie

---
+++ hoofdstuk4/Instructie/while&for.php
@@ -0,0 +1,19 @@
+<?php
+// While & For Loop
+//$counter = 0;
+//$tekst = "";
+//while($counter <= 10)
+//{
+////    $tekst .= "Hoi" . $counter . "<br>";
+//    $tekst .= $tekst . "Hoi" . $counter . "<br>";
+//    $counter++;
+//}
+//
+//echo $tekst;
+
+for($teller=0 ;$teller <=10; $teller++)
+{
+    echo "wat" . $teller . "<br>";
+}
+
+?>
