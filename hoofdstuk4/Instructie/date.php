a2e8735ffaba49f27ef63f2dcea5448115327486
Lucas <lucas.kooijmans@edu-kw1c.nl>
2020-03-09 12:34:15 +0100

    instructie

--- hoofdstuk4/Instructie/date.php
+++ hoofdstuk4/Instructie/date.php
@@ -1,24 +1,5 @@
 <?php
-/**
- * User: Lucas Kooijmans
- * Date:
- * Time:
- * File:
- */
-?>
 
-    <!-- Include header -->
-<?php
-include "../../includes/header.php";
-?>
-
-    <!-- Include menu -->
-<?php
-include "../../includes/menu.php";
-?>
-
-    <!-- Uitwerking -->
-<?php
 date_default_timezone_set("Europe/Amsterdam");
 //echo date("d-m-Y H:i");
 $month = date("n");
@@ -32,9 +13,4 @@ elseif ($month >= 6 && $month <= 9)
     echo "Het is zomer";
 }
 
-?>
-
-    <!-- Include footer -->
-<?php
-include "../../includes/footer.php";
 ?>
